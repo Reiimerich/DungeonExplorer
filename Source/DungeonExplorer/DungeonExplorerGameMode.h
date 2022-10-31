ca708ecd6a9c4af7c3d876f917b07e5c95ccfbf0
Joseph Bermudez <106097275+Reiimerich@users.noreply.github.com>
2022-10-31 12:03:14 -0600

    Start Project II

---
+++ Source/DungeonExplorer/DungeonExplorerGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DungeonExplorerGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADungeonExplorerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADungeonExplorerGameMode();
+};
+
+
+
